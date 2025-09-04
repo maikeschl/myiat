fdfddbfdac14603f977d6b51c50ee15b57b1a7f3
Maike Schlosser <147407329+maikeschl@users.noreply.github.com>
2025-09-04 16:03:30 +0200

    Update iat.js

--- iat.js
+++ iat.js
@@ -1,28 +1,26 @@
-define(['pipAPI','https://baranan.github.io/minno-tasks/quiat8.js'], 
+define(['pipAPI','https://baranan.github.io/minno-tasks/quiat8.js'],
 function(APIConstructor, iatExtension){
   var API = new APIConstructor();
   return iatExtension({
-    // isTouch: true, // uncomment if you allow phones/tablets
+    isTouch: true, // enables tap targets & touch instructions
 
     category1: {
       name: 'Black people',
       title: { media:{word:'Black people'}, css:{color:'#31940F','font-size':'2em'}, height:4 },
       stimulusMedia: [
-        {word:'WordA1'},{word:'WordA2'},{word:'WordA3'},{word:'WordA4'},{word:'WordA5'},{word:'WordA6'}
+        {word:'WordA1'},{word:'WordA2'},{word:'WordA3'},
+        {word:'WordA4'},{word:'WordA5'},{word:'WordA6'}
       ],
       stimulusCss: {color:'#31940F','font-size':'1.8em'}
     },
-
     category2: {
       name: 'White people',
       title: { media:{word:'White people'}, css:{color:'#31940F','font-size':'2em'}, height:4 },
       stimulusMedia: [
-        {word:'WordB1'},{word:'WordB2'},{word:'WordB3'},{word:'WordB4'},{word:'WordB5'},{word:'WordB6'}
+        {word:'WordB1'},{word:'WordB2'},{word:'WordB3'},
+        {word:'WordB4'},{word:'WordB5'},{word:'WordB6'}
       ],
       stimulusCss: {color:'#31940F','font-size':'1.8em'}
-    },
-
-    // No base_url needed when using words only
-    // base_url: { image: '' }
+    }
   });
 });
